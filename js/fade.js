639c626e8edf6c73d8cbca18c839a68e51f17675
Mitchell Vitez <mitchell@vitez.me>
2014-11-23 12:17:04 -0500

    Update

--- js/fade.js
+++ js/fade.js
@@ -1,26 +1,23 @@
-
-      jQuery(function( $ ){ // DOM ready shorthand
-    
+/*  Creates fading slideshow of items with class "symbol"
+    By Mitchell Vitez. Feel free to use with attribution
+*/
+jQuery(function( $ ){
     (function() {
-
-var symbol = $(".symbol");
-var symbolIndex = 0;
-var oldSymbolIndex;
-var oldoldSymbolIndex;
-function showNextsymbol() {
-    oldoldSymbolIndex = oldSymbolIndex;
-    oldSymbolIndex = symbolIndex;
-    do
-      symbolIndex = Math.floor(Math.random() * (symbol.length - 0)) + 0;
-    while (oldSymbolIndex == symbolIndex || oldoldSymbolIndex == symbolIndex);
-    symbol.eq(symbolIndex % symbol.length)
-        .fadeIn(500)
-        .delay(2000)
-        .fadeOut(500, showNextsymbol);
-}
-
-showNextsymbol();
-
-})();
-     
+        var symbol = $(".symbol");
+        var symbolIndex = 0;
+        var oldSymbolIndex;
+        var oldoldSymbolIndex;
+        function showNextsymbol() {
+            oldoldSymbolIndex = oldSymbolIndex;
+            oldSymbolIndex = symbolIndex;
+            do
+                symbolIndex = Math.floor(Math.random() * (symbol.length - 0)) + 0;
+            while (oldSymbolIndex == symbolIndex || oldoldSymbolIndex == symbolIndex);
+            symbol.eq(symbolIndex % symbol.length)
+            .fadeIn(500)
+            .delay(2000)
+            .fadeOut(500, showNextsymbol);
+        }
+        showNextsymbol();
+    })(); 
 });
