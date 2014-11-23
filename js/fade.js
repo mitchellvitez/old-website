147e0e38fe948a686c5e2a741fa5804cb2b4a186
Mitchell Vitez <mitchell@vitez.me>
2014-11-23 12:12:48 -0500

    Translated html to jade so jade templates can be used in the future

---
+++ js/fade.js
@@ -0,0 +1,26 @@
+
+      jQuery(function( $ ){ // DOM ready shorthand
+    
+    (function() {
+
+var symbol = $(".symbol");
+var symbolIndex = 0;
+var oldSymbolIndex;
+var oldoldSymbolIndex;
+function showNextsymbol() {
+    oldoldSymbolIndex = oldSymbolIndex;
+    oldSymbolIndex = symbolIndex;
+    do
+      symbolIndex = Math.floor(Math.random() * (symbol.length - 0)) + 0;
+    while (oldSymbolIndex == symbolIndex || oldoldSymbolIndex == symbolIndex);
+    symbol.eq(symbolIndex % symbol.length)
+        .fadeIn(500)
+        .delay(2000)
+        .fadeOut(500, showNextsymbol);
+}
+
+showNextsymbol();
+
+})();
+     
+});
